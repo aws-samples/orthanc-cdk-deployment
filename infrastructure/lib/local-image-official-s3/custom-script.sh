750ab456d9c7f060058731920664e48cf9fb5724
Tamas Santa <contact@tamas-santa.com>
2026-07-28 15:39:35 +0100

    removed custom docker image

--- infrastructure/lib/local-image-official-s3/custom-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-set -o xtrace
-
-# Removing S3 plugin so it won't crash Orthanc at startup, if the configuration is not present
-rm -f /usr/share/orthanc/plugins/libOrthancAwsS3Storage.so
